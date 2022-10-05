5a1a4c65d738d993a8dcebdf1865e87cf6d9d52e
Cristian Palacios <113551947+cpalacios3@users.noreply.github.com>
2022-10-05 19:05:55 +0000

    add a list with for loops

--- loops.py
+++ loops.py
@@ -107,7 +107,8 @@ def loop():
   # For example: "Hello Norville"
   
   students = ["Norville", "Fred", "Velma", "Daphne"]
-  
+  for name in students:
+    print(f"Hello, {name}")
   
   
   
@@ -115,7 +116,11 @@ def loop():
   # Given the following list of numbers, calculate the sum of all the numbers using For loops and store the result of the sum in a variable called sum_numbers:
   
   list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
-  # sum_numbers = 
+  sum_numbers = 0
+  for number in list_numbers:
+    sum_numbers = sum_numbers + number
+    print(f"Sum of all list numbers is {sum_numbers}")
+  
   
   
   
